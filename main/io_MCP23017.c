a098954eacb7db4c45c583577b085e9b5b9dfc79
MiraFeatherbender <jonathangorum813@gmail.com>
2026-01-30 10:44:02 -0600

    fix: correct initialization of I2C bus configuration flags

--- main/io_MCP23017.c
+++ main/io_MCP23017.c
@@ -94,8 +94,10 @@ static void io_mcp23017_task(void *arg)
         .glitch_ignore_cnt = 7,
         .intr_priority = 0,
         .trans_queue_depth = 0,
+        .flags{
+            enable_internal_pullup = 1,
+        },
     };
-    bus_cfg.flags.enable_internal_pullup = 1;
     ESP_LOGI(TAG, "Using example I2C bus config: SDA=%d SCL=%d", bus_cfg.sda_io_num, bus_cfg.scl_io_num);
 
     esp_err_t rc = mcp23017_auto_setup(&s_mcp_devices, true, &bus_cfg);
